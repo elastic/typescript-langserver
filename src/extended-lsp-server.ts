5d68fc1b99f323ea7616d02cab335e8b579d2267
Fuyao Zhao <zfy0701@gmail.com>
2019-04-02 13:15:21 -0700

    Path filtering for references and hover

--- src/extended-lsp-server.ts
+++ src/extended-lsp-server.ts
@@ -1,12 +1,16 @@
+import { DependencyManager } from './dependency-manager';
+
 import { DetailSymbolInformation, Full, FullParams } from '@elastic/lsp-extension';
 import { collectSymbolInformations } from '@elastic/typescript-language-server/lib/document-symbol';
 import { LspServer } from '@elastic/typescript-language-server/lib/lsp-server';
-import { uriToPath } from '@elastic/typescript-language-server/lib/protocol-translation';
+import { asRange, asTagsDocumentation, uriToPath } from '@elastic/typescript-language-server/lib/protocol-translation';
 import { TypeScriptInitializeParams } from '@elastic/typescript-language-server/lib/ts-protocol';
 import { CommandTypes } from '@elastic/typescript-language-server/lib/tsp-command-types';
 
+import * as path from 'path';
 import * as lsp from 'vscode-languageserver';
-import {DependencyManager} from './dependency-manager';
+
+const NODE_MODULES: string = 'node_modules/' + path.sep;
 
 export class ExtendedLspServer extends LspServer {
   private gitHostWhitelist: string[] | undefined;
@@ -16,20 +20,40 @@ export class ExtendedLspServer extends LspServer {
     return super.documentSymbol(params);
   }
 
-  hover(params: lsp.TextDocumentPositionParams) {
+  async hover(params: lsp.TextDocumentPositionParams): Promise<lsp.Hover> {
     this.ensureDocumentOpen(params.textDocument.uri);
-    // TODO: security filtering
-    return super.hover(params);
+    const file = uriToPath(params.textDocument.uri);
+    this.logger.log('hover', params, file);
+    if (!file) {
+      return { contents: [] };
+    }
+
+    const result = await this.interuptDiagnostics(() => this.getQuickInfo(file, params.position));
+    if (!result || !result.body) {
+      return { contents: [] };
+    }
+    const range = asRange(result.body);
+    const contents: lsp.MarkedString[] = [
+      { language: 'typescript', value: this.replaceWorkspaceInString(result.body.displayString) },
+    ];
+    // TODO: security filtering for documentation
+    const tags = asTagsDocumentation(result.body.tags);
+    contents.push(result.body.documentation + (tags ? '\n\n' + tags : ''));
+    return {
+      contents,
+      range,
+    };
   }
 
-  references(params: lsp.TextDocumentPositionParams) {
+  async references(params: lsp.TextDocumentPositionParams) {
     this.ensureDocumentOpen(params.textDocument.uri);
-    // TODO: filter current project
-    return super.references(params);
+    const rawResult = await super.references(params);
+    return rawResult.filter((location) => {
+      return location.uri.indexOf(NODE_MODULES) === -1;
+    });
   }
 
   initialize(params: TypeScriptInitializeParams) {
-    // TODO: install deps
     const dependencyManager = new DependencyManager(params.rootPath || uriToPath(params.rootUri!));
     if (params.initializationOptions.installNodeDependency) {
       dependencyManager.installDependency();
@@ -112,5 +136,11 @@ export class ExtendedLspServer extends LspServer {
   private ensureDocumentOpen(uri: string) {
     // TODO check if languageId does matter
     this.didOpenTextDocument({ textDocument: { uri, languageId: '', text: '', version: 0 }});
+  }
+
+  private replaceWorkspaceInString(str: string): string {
+    const withoutRoot = str.replace(this.rootPath(), '');
+    const res = withoutRoot.split(NODE_MODULES);
+    return res[res.length - 1];
   }
 }
